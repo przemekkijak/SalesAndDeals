c660f9c375e415f92c14bd043cd066d2b2653171
przemekkijak <przemekijak@gmail.com>
2021-02-03 20:44:16 +0100

    displaying todo scrapers for user

--- client/src/app/_components/scrapers/my-scrapers/todo-scrapers/todo-scrapers.component.ts
+++ client/src/app/_components/scrapers/my-scrapers/todo-scrapers/todo-scrapers.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import { FetchService } from 'src/app/_services/fetch.service';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-todo-scrapers',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-scrapers.component.scss']
 })
 export class TodoScrapersComponent implements OnInit {
+  displayedColumns: string[] = ['name','lastExecuted','notes','inputUrl','dexiRobot', 'dexiExecutions'];
+  scrapers: any = [];
+  dataSource = new MatTableDataSource<any>();
 
-  constructor() { }
+  constructor(private fetch: FetchService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
+    this.fetch.getScrapersTodo(this.tokenStorage.getUser().id).subscribe(res => {
+      this.scrapers = res;
+      this.dataSource = this.scrapers;
+    })
+  }
+
+  openNotes(shopId: number) {
+    console.log('notes');
   }
 
 }
