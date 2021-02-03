e0485bb02c76fc6b5280dc9e1b1fbac09fb47f38
przemekkijak <przemekijak@gmail.com>
2021-02-03 20:51:27 +0100

    mark shop as in scrapers view

--- client/src/app/_components/scrapers/my-scrapers/todo-scrapers/todo-scrapers.component.ts
+++ client/src/app/_components/scrapers/my-scrapers/todo-scrapers/todo-scrapers.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import { FetchService } from 'src/app/_services/fetch.service';
 import { TokenStorageService } from 'src/app/_services/token-storage.service';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-todo-scrapers',
@@ -10,9 +11,13 @@ import { TokenStorageService } from 'src/app/_services/token-storage.service';
   styleUrls: ['./todo-scrapers.component.scss']
 })
 export class TodoScrapersComponent implements OnInit {
-  displayedColumns: string[] = ['name','lastExecuted','notes','inputUrl','dexiRobot', 'dexiExecutions'];
+  displayedColumns: string[] = ['name','lastExecuted','notes','inputUrl','dexiRobot', 'dexiExecutions','markAs'];
   scrapers: any = [];
   dataSource = new MatTableDataSource<any>();
+
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+  @ViewChild(MatPaginator) paginator: MatPaginator
+
 
   constructor(private fetch: FetchService, private tokenStorage: TokenStorageService) { }
 
