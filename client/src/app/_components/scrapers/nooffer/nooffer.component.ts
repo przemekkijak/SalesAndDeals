a3a5c81c8b873918f531f3fb015b2e387488330d
przemekkijak <przemekijak@gmail.com>
2021-02-07 13:55:05 +0100

    no offer added

--- client/src/app/_components/scrapers/nooffer/nooffer.component.ts
+++ client/src/app/_components/scrapers/nooffer/nooffer.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatTableDataSource } from '@angular/material/table';
+import { FetchService } from 'src/app/_services/fetch.service';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-nooffer',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nooffer.component.scss']
 })
 export class NoofferComponent implements OnInit {
+  displayedColumns: string[] = ['name','lastExecuted','actions'];
+  scrapers: any = [];
+  dataSource = new MatTableDataSource<any>();
 
-  constructor() { }
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+
+  constructor(private fetch: FetchService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
+    this.fetch.getScrapers(0, "NOOFFER").subscribe(res => {
+      this.scrapers = res;
+      this.dataSource = this.scrapers;
+    })
   }
-
 }
