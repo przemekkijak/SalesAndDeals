8e0578b51e0907820d89805a102d13335202d8f6
przemekkijak <przemekijak@gmail.com>
2021-02-09 18:21:29 +0100

    separated scrapers view as routes - easier to refresh data

--- client/src/app/_components/scrapers/my-scrapers/my-scrapers.component.ts
+++ client/src/app/_components/scrapers/my-scrapers/my-scrapers.component.ts
@@ -9,6 +9,13 @@ export class MyScrapersComponent implements OnInit {
 
   constructor() { }
 
+  links = [
+    {name: 'Todo', route: 'todo'},
+    {name: 'Executed', route: 'executed'},
+    {name: 'Success', route: 'success'}
+  ];
+  activeLink = this.links[0];
+
   ngOnInit(): void {
   }
 
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,14 +4,30 @@ import { NotesComponent } from './_components/notes/notes.component';
 import { LoginComponent } from './_components/login/login.component';
 import { ShopsComponent } from './_components/shops/shops.component';
 import {ScrapersComponent} from './_components/scrapers/scrapers.component';
-
 import {AuthGuard} from './_helpers/auth.guard';
+import { TodoScrapersComponent } from './_components/scrapers/my-scrapers/todo-scrapers/todo-scrapers.component';
+import { WaitExecutionScrapersComponent } from './_components/scrapers/my-scrapers/wait-execution-scrapers/wait-execution-scrapers.component';
+import { SuccessScrapersComponent } from './_components/scrapers/my-scrapers/success-scrapers/success-scrapers.component';
 
 
 const routes: Routes = [
-  {path: 'scrapers', component: ScrapersComponent, canActivate: [AuthGuard]},
-  {path: 'shops', component: ShopsComponent, canActivate: [AuthGuard]},
-  {path: 'notes', component: NotesComponent, canActivate: [AuthGuard]},
+  {
+    path: 'scrapers', 
+    component: ScrapersComponent, 
+    canActivate: [AuthGuard],
+    children: [
+    {path: 'todo', component: TodoScrapersComponent},
+    {path: 'executed', component: WaitExecutionScrapersComponent},
+    {path: 'success', component: SuccessScrapersComponent}
+  ]},
+  
+  {path: 'shops', 
+  component: ShopsComponent, 
+  canActivate: [AuthGuard]},
+
+  {path: 'notes', 
+  component: NotesComponent, 
+  canActivate: [AuthGuard]},
 
 
 
