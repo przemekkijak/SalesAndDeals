31cd4ecc2d920bd183e796c8bac510b1a440f81e
przemekkijak <przemekijak@gmail.com>
2021-02-18 12:49:33 +0100

    routing to admin paths

--- client/src/app/_components/scrapers/scrapers-admin/admin-main/admin-main.component.ts
+++ client/src/app/_components/scrapers/scrapers-admin/admin-main/admin-main.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatTableDataSource } from '@angular/material/table';
+import { FetchService } from 'src/app/_services/fetch.service';
 
 @Component({
   selector: 'app-admin-main',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-main.component.scss']
 })
 export class AdminMainComponent implements OnInit {
+  displayedColumns: string[] = ['name','lastExecuted','scraper', 'actions'];
+  success: any = [];
+  cant: any = [];
+  successDataSource = new MatTableDataSource<any>();
+  cantDataSource = new MatTableDataSource<any>();
 
-  constructor() { }
+
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+
+  constructor(private fetch: FetchService) { }
 
   ngOnInit(): void {
-  }
+    this.fetch.getScrapers(0, "SUCCESS").subscribe(res => {
+      this.success = res;
+      this.success.sort((a,b) => a.name.localeCompare(b.name));
+      this.successDataSource = this.success;
+      console.log(this.success);
+    })
+    this.fetch.getScrapers(0, "CANTDOTHIS").subscribe(res => {
+      this.cant = res;
+      this.cantDataSource = this.cant;
 
+    })
+  }
 }
--- client/src/app/_components/scrapers/scrapers-admin/scrapers-admin.component.ts
+++ client/src/app/_components/scrapers/scrapers-admin/scrapers-admin.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatMenuTrigger } from '@angular/material/menu';
-import { MatTableDataSource } from '@angular/material/table';
-import { FetchService } from 'src/app/_services/fetch.service';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-scrapers-admin',
@@ -9,28 +6,16 @@ import { FetchService } from 'src/app/_services/fetch.service';
   styleUrls: ['./scrapers-admin.component.scss']
 })
 export class ScrapersAdminComponent implements OnInit {
-  displayedColumns: string[] = ['name','lastExecuted','scraper', 'actions'];
-  success: any = [];
-  cant: any = [];
-  successDataSource = new MatTableDataSource<any>();
-  cantDataSource = new MatTableDataSource<any>();
 
+  constructor() { }
 
-  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
-
-  constructor(private fetch: FetchService) { }
+  links = [
+    {name: 'Main', route: 'main'},
+    {name: 'Overview', route: 'overview'},
+  ];
+  activeLink = this.links[0];
 
   ngOnInit(): void {
-    this.fetch.getScrapers(0, "SUCCESS").subscribe(res => {
-      this.success = res;
-      this.success.sort((a,b) => a.name.localeCompare(b.name));
-      this.successDataSource = this.success;
-      console.log(this.success);
-    })
-    this.fetch.getScrapers(0, "CANTDOTHIS").subscribe(res => {
-      this.cant = res;
-      this.cantDataSource = this.cant;
-
-    })
   }
+
 }
